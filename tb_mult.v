ca3f7522a67cfea2a840542e35a56152cbe42dd7
Adam Stenson <AdamStenson04@gmail.com>
2016-04-04 17:28:16 -0400

    Working on the mult unit

--- tb_mult.v
+++ tb_mult.v
@@ -8,5 +8,11 @@ module tb_mult;
   initial begin
     A = 16'b1010_1010_1010_1010;
     B = 16'b1111_1111_1111_1111;
+    #100
+    A = 16'b0000_0000_0000_0010;
+    B = 16'b0000_0000_0000_0100;
+  end
+  initial begin
+    #100 $monitor("A=%b B=%b : result=%b",A,B,result);
   end
 endmodule
