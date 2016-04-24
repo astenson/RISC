e17901f20621241c12ff030119ad7b13254efcf3
Adam Stenson <AdamStenson04@gmail.com>
2016-04-24 00:27:04 -0400

    Fixed the adjust PC datapath

--- main.v
+++ main.v
@@ -1,3 +1,19 @@
+/*
+Master ToDo list:
+TODO Make a new instruction memrory that tests every command and make a table of the expected outputs
+TODO Add the zero output to the ALU (for branch commands)
+TODO Implement division (div)
+TODO Implement multipication (multi)
+TODO Implement mfhi
+TODO Implement mflo
+TODO Implement sll
+TODO Implement slr
+TODO Implement jr
+TODO Implement jal
+TODO Write factorial assembly code
+TODO Write factorial machine code into a new instruction memrory file
+*/
+
 module full_adder(x,y,cin,s,cout);
 	input x,y,cin;
 	output s,cout;
@@ -14,8 +30,8 @@ module clock(output clk);
   reg clk;
   initial begin
     repeat( 18 ) begin
-      clk = 1'b0;
-      #1000000 clk = ~clk;
+			clk = 0;
+      #100 clk = !clk;
     end
   end
 endmodule
@@ -29,8 +45,8 @@ module mux(input A, input B, input sel, output out);
 	or(out,D,E);
 endmodule
 
-module adder16bit(input [15:0] A, input [15:0] B, output [15:0] result);
-  wire [16:0] carry;
+module adder32bit(input [31:0] A, input [31:0] B, output [31:0] result);
+  wire [32:0] carry;
   assign carry[0] = 0;
   full_adder adder1(.x(A[0]),.y(B[0]),.cin(carry[0]),.s(result[0]),.cout(carry[1]));
   full_adder adder2(.x(A[1]),.y(B[1]),.cin(carry[1]),.s(result[1]),.cout(carry[2]));
@@ -47,7 +63,23 @@ module adder16bit(input [15:0] A, input [15:0] B, output [15:0] result);
   full_adder adder13(.x(A[12]),.y(B[12]),.cin(carry[12]),.s(result[12]),.cout(carry[13]));
   full_adder adder14(.x(A[13]),.y(B[13]),.cin(carry[13]),.s(result[13]),.cout(carry[14]));
   full_adder adder15(.x(A[14]),.y(B[14]),.cin(carry[14]),.s(result[14]),.cout(carry[15]));
-  full_adder adder16(.x(A[15]),.y(B[15]),.cin(carry[15]),.s(result[15]),.cout(carry[16]));
+	full_adder adder16(.x(A[15]),.y(B[15]),.cin(carry[15]),.s(result[15]),.cout(carry[16]));
+  full_adder adder17(.x(A[16]),.y(B[16]),.cin(carry[16]),.s(result[16]),.cout(carry[17]));
+	full_adder adder18(.x(A[17]),.y(B[17]),.cin(carry[17]),.s(result[17]),.cout(carry[18]));
+	full_adder adder19(.x(A[18]),.y(B[18]),.cin(carry[18]),.s(result[18]),.cout(carry[19]));
+	full_adder adder20(.x(A[19]),.y(B[19]),.cin(carry[19]),.s(result[19]),.cout(carry[20]));
+	full_adder adder21(.x(A[20]),.y(B[20]),.cin(carry[20]),.s(result[20]),.cout(carry[21]));
+	full_adder adder22(.x(A[21]),.y(B[21]),.cin(carry[21]),.s(result[21]),.cout(carry[22]));
+	full_adder adder23(.x(A[22]),.y(B[22]),.cin(carry[22]),.s(result[22]),.cout(carry[23]));
+	full_adder adder24(.x(A[23]),.y(B[23]),.cin(carry[23]),.s(result[23]),.cout(carry[24]));
+	full_adder adder25(.x(A[24]),.y(B[24]),.cin(carry[24]),.s(result[24]),.cout(carry[25]));
+	full_adder adder26(.x(A[25]),.y(B[25]),.cin(carry[25]),.s(result[25]),.cout(carry[26]));
+	full_adder adder27(.x(A[26]),.y(B[26]),.cin(carry[26]),.s(result[26]),.cout(carry[27]));
+	full_adder adder28(.x(A[27]),.y(B[27]),.cin(carry[27]),.s(result[27]),.cout(carry[28]));
+	full_adder adder29(.x(A[28]),.y(B[28]),.cin(carry[28]),.s(result[28]),.cout(carry[29]));
+	full_adder adder30(.x(A[29]),.y(B[29]),.cin(carry[29]),.s(result[29]),.cout(carry[30]));
+	full_adder adder31(.x(A[30]),.y(B[30]),.cin(carry[30]),.s(result[30]),.cout(carry[31]));
+	full_adder adder32(.x(A[31]),.y(B[31]),.cin(carry[31]),.s(result[31]),.cout(carry[32]));
 endmodule
 
 module ALU(input A, input B, input Cin, input [3:0] sel, input less, output out, output Cout, output set);
@@ -119,39 +151,21 @@ module dataMem(input clk, input write, input read, input [15:0] Addr, input [15:
   end
 endmodule
 
-module instructMem(input clk, input [15:0] addr, output [31:0] instruction);
+module instructMem(input clk, input [31:0] addr, output [31:0] instruction);
   reg [31:0] temp_instruct;
-  assign instruction = temp_instruct;
 
   reg [7:0] instructions[0:71];
 
   initial begin
     $readmemb("instructions.txt",instructions);
   end
-  always @ ( posedge clk ) begin
+  always @* begin
     temp_instruct[31:24] <= instructions[addr];
     temp_instruct[23:16] <= instructions[addr + 1];
     temp_instruct[15:8] <= instructions[addr + 2];
     temp_instruct[7:0] <= instructions[addr + 3];
   end
-
-endmodule
-
-module PCadjust(input clk, input [15:0] PC, input [15:0] jumpAddr, input [1:0] select, output [15:0] adjustedPC);
-  wire [15:0] A, B;
-  reg [15:0] four = 16'b0000_0000_0000_0100;
-  reg [15:0] temp;
-  assign adjustedPC = temp;
-  adder16bit add1(.A(PC),.B(four),.result(A));
-  adder16bit add2(.A(A),.B(jumpAddr),.result(B));
-  always @ ( negedge clk ) begin
-    case (select)
-      2'b00: begin temp = A; end
-      2'b01: begin temp = jumpAddr; end
-      2'b10: begin temp = B; end
-      default : $display("Error in select");
-    endcase
-  end
+	assign instruction = temp_instruct;
 
 endmodule
 
@@ -185,8 +199,8 @@ module regfile(clk, rs, rt, rd, data_in, A, B);
   end
 endmodule
 
-module shiftLeft(input [15:0] in, output [15:0] out);
-  assign out[15:2] = in[13:0];
+module shiftLeft(input [31:0] in, output [31:0] out);
+  assign out[31:2] = in[29:0];
   assign out[1] = 0;
   assign out[0] = 0;
 endmodule
@@ -204,49 +218,93 @@ module mux16bit(sel, i1, i2, o1);
   end
 endmodule
 
-module mux3to1(sel, i1, i2, i3, o1);
-  input [1:0] sel;
-  input [15:0] i1, i2, i3;
-  output [15:0] o1;
-  reg [15:0] o1;
+module mux3to1_1bit(input [1:0] sel, input in1, input in2, input in3, output out);
+  wire notSel1, notSel2, A, B, C;
+  not(notSel1, sel[0]);
+  not(notSel2, sel[1]);
+  and(A, in1, notSel1, notSel2);
+  and(B, in2, notSel2, sel[0]);
+  and(C, in3, sel[1], notSel1);
+  or(out, A, B, C);
+endmodule
 
-  always @(sel or i1 or i2 or i3) begin
-    case (sel)
-      2'b00: o1 = i1;
-      2'b01: o1 = i2;
-      2'b10: o1 = i3;
-    endcase
-  end
+module mux3to1(input [1:0] sel, input [31:0] A, input [31:0] B, input [31:0] C, output [31:0] result);
+  mux3to1_1bit mux1(sel, A[0], B[0], C[0], result[0]);
+  mux3to1_1bit mux2(sel, A[1], B[1], C[1], result[1]);
+  mux3to1_1bit mux3(sel, A[2], B[2], C[2], result[2]);
+  mux3to1_1bit mux4(sel, A[3], B[3], C[3], result[3]);
+  mux3to1_1bit mux5(sel, A[4], B[4], C[4], result[4]);
+  mux3to1_1bit mux6(sel, A[5], B[5], C[5], result[5]);
+  mux3to1_1bit mux7(sel, A[6], B[6], C[6], result[6]);
+  mux3to1_1bit mux8(sel, A[7], B[7], C[7], result[7]);
+  mux3to1_1bit mux9(sel, A[8], B[8], C[8], result[8]);
+  mux3to1_1bit mux10(sel, A[9], B[9], C[9], result[9]);
+  mux3to1_1bit mux11(sel, A[10], B[10], C[10], result[10]);
+  mux3to1_1bit mux12(sel, A[11], B[11], C[11], result[11]);
+  mux3to1_1bit mux13(sel, A[12], B[12], C[12], result[12]);
+  mux3to1_1bit mux14(sel, A[13], B[13], C[13], result[13]);
+  mux3to1_1bit mux15(sel, A[14], B[14], C[14], result[14]);
+  mux3to1_1bit mux16(sel, A[15], B[15], C[15], result[15]);
+  mux3to1_1bit mux17(sel, A[16], B[16], C[16], result[16]);
+  mux3to1_1bit mux18(sel, A[17], B[17], C[17], result[17]);
+  mux3to1_1bit mux19(sel, A[18], B[18], C[18], result[18]);
+  mux3to1_1bit mux20(sel, A[19], B[19], C[19], result[19]);
+  mux3to1_1bit mux21(sel, A[20], B[20], C[20], result[20]);
+  mux3to1_1bit mux22(sel, A[21], B[21], C[21], result[21]);
+  mux3to1_1bit mux23(sel, A[22], B[22], C[22], result[22]);
+  mux3to1_1bit mux24(sel, A[23], B[23], C[23], result[23]);
+  mux3to1_1bit mux25(sel, A[24], B[24], C[24], result[24]);
+  mux3to1_1bit mux26(sel, A[25], B[25], C[25], result[25]);
+  mux3to1_1bit mux27(sel, A[26], B[26], C[26], result[26]);
+  mux3to1_1bit mux28(sel, A[27], B[27], C[27], result[27]);
+  mux3to1_1bit mux29(sel, A[28], B[28], C[28], result[28]);
+  mux3to1_1bit mux30(sel, A[29], B[29], C[29], result[29]);
+  mux3to1_1bit mux31(sel, A[30], B[30], C[30], result[30]);
+  mux3to1_1bit mux32(sel, A[31], B[31], C[31], result[31]);
+endmodule
+
+module padding16to32(input [15:0] in, output [31:0] out);
+	assign out[15:0] = in;
+	assign out[31:16] = 16'b0000_0000_0000_0000;
+endmodule
+
+module padding26to32(input [25:0] in, output [31:0] out);
+	assign out[25:0] = in;
+	assign out[31:26] = 6'b00_0000;
 endmodule
 
 module control;
 	wire clk, overflow;
-	reg [1:0] mux2Select;
-  wire [15:0] temp_PC, rdData1, rdData2, ALUIn, writeRegData, PCplus4, jumpAddr, ALUResult, branchAddr, rdDataMem;
+	reg [1:0] mux2Select = 2'b00;
+	wire [31:0] temp_PC;
+  wire [15:0] rdData1, rdData2, ALUIn, writeRegData, ALUResult, rdDataMem;
 	reg RegWrite, ALUSrc, PCSrc, MemRead, MemWrite, MemtoReg, half; //if loading half then half = 1, if loading byte half = 0
 	reg [3:0] ALUOpCode;
-	reg [15:0] PC = 16'b0000_0000_0000_0000;
-	reg [15:0] four = 16'b0000_0000_0000_0100;
-	wire [31:0] instruction;
+	reg [31:0] PC = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+	reg [31:0] four = 32'b0000_0000_0000_0000_0000_0000_0000_0100;
+	wire [31:0] instruction, shiftedJA, shiftedBA, PCplus4, tempJA, tempBA, branchAddr;
 	reg [5:0] opcode, funct;
 	reg [4:0] writeAddr;
 
 	clock clk1(.clk(clk));
+	padding16to32 padding1(instruction[15:0], tempBA);
+	padding26to32 padding2(instruction[25:0], tempJA);
+	shiftLeft shift1(tempJA, shiftedJA);
+	shiftLeft shift2(tempBA, shiftedBA);
+	adder32bit adder2(PCplus4, shiftedBA, branchAddr);
 	instructMem instruct(clk, PC, instruction);
-	adder16bit adder1(PC, four, PCplus4);
+	adder32bit adder1(PC, four, PCplus4);
 	regfile regRead(clk, instruction[25:21], instruction[20:16], writeAddr, writeRegData, rdData1, rdData2);
 	mux16bit mux1(ALUSrc, instruction[15:0], rdData2, ALUIn);
-	shiftLeft shift1(instruction[15:0], jumpAddr);
 	ALU16bit alu1(rdData1, ALUIn, ALUOpCode, ALUResult, overflow);
-	adder16bit adder2(PCplus4, jumpAddr, branchAddr);
-	mux3to1 mux2(mux2Select, PCplus4, jumpAddr, branchAddr, temp_PC);
 	dataMem data(clk, MemWrite, MemRead, ALUResult, rdData2, rdDataMem);
 	mux16bit mux3(MemtoReg, rdDataMem, ALUResult, writeRegData);
+	mux3to1 mux2(mux2Select, PCplus4, shiftedJA, branchAddr, temp_PC);
 	always @ ( posedge clk ) begin
-		mux2Select = 2'b00;
 		ALUSrc = 1'b1;
 		MemtoReg = 1;
 		funct = instruction[5:0];
+		opcode = instruction[31:25];
 		case (opcode)
 			6'b00_0000 : begin //R-type instructions
 											funct = instruction[5:0];
@@ -256,6 +314,7 @@ module control;
 											MemWrite = 1'b0;
 											MemtoReg = 1'b1;
 											writeAddr = instruction[15:11];
+											mux2Select = 2'b00;
 									 end
 			6'b00_1000 : begin //addi
 											funct = 6'b10_0000;
@@ -265,6 +324,7 @@ module control;
 											MemWrite = 1'b0;
 											MemtoReg = 1'b1;
 											writeAddr = instruction[20:16];
+											mux2Select = 2'b00;
 									 end
 			6'b00_1100 : begin //andi
 											funct = 6'b10_0100;
@@ -274,6 +334,7 @@ module control;
 											MemWrite = 1'b0;
 											MemtoReg = 1'b1;
 											writeAddr = instruction[20:16];
+											mux2Select = 2'b00;
 									 end
 			6'b00_1101 : begin //ori
 											funct = 6'b10_0101;
@@ -283,6 +344,7 @@ module control;
 											MemWrite = 1'b0;
 											MemtoReg = 1'b1;
 											writeAddr = instruction[20:16];
+											mux2Select = 2'b00;
 									 end
 			6'b00_1111 : begin //lui
 											funct = 6'b10_0000;
@@ -292,6 +354,7 @@ module control;
 											MemWrite = 1'b0;
 											MemtoReg = 1'b1;
 											writeAddr = instruction[20:16];
+											mux2Select = 2'b00;
 									 end
 				6'b00_1010 : begin //slti
 											funct = 6'b10_1010;
@@ -301,6 +364,7 @@ module control;
 											MemWrite = 1'b0;
 											MemtoReg = 1'b1;
 											writeAddr = instruction[20:16];
+											mux2Select = 2'b00;
 										 end
 				6'b00_0100 : begin //beq
 											funct = 6'b10_0010;
@@ -310,6 +374,7 @@ module control;
 											MemWrite = 1'b0;
 											MemtoReg = 1'b1;
 											writeAddr = instruction[15:11];
+											mux2Select = 2'b10;
 										 end
 				6'b00_0001 : begin //bqez
  										  funct = 6'b10_0010;
@@ -319,6 +384,7 @@ module control;
  											MemWrite = 1'b0;
  											MemtoReg = 1'b1;
  											writeAddr = instruction[15:11];
+											mux2Select = 2'b10;
  										 end
 				6'b00_0010 : begin //j
 				 							funct = 6'b10_0000;
@@ -328,6 +394,7 @@ module control;
 				 							MemWrite = 1'b0;
 				 							MemtoReg = 1'b1;
 				 							writeAddr = instruction[15:11];
+											mux2Select = 2'b01;
 				 						 end
 				6'b10_0000 : begin //lb
  											funct = 6'b10_0000;
@@ -338,6 +405,7 @@ module control;
  											MemtoReg = 1'b0;
  											writeAddr = instruction[20:16];
 											half = 1'b0;
+											mux2Select = 2'b00;
  										 end
 			  6'b10_0001 : begin //lh
 											funct = 6'b10_0000;
@@ -348,6 +416,7 @@ module control;
 											MemtoReg = 1'b0;
 											writeAddr = instruction[20:16];
 											half = 1'b1;
+											mux2Select = 2'b00;
 										 end
 				6'b10_1000 : begin //sb
  											funct = 6'b10_0000;
@@ -358,8 +427,9 @@ module control;
  											MemtoReg = 1'b0;
  											writeAddr = instruction[20:16];
  											half = 1'b0;
+											mux2Select = 2'b00;
  										 end
-			  6'b10_1001 : begin //sb
+			  6'b10_1001 : begin //sh
 											funct = 6'b10_0000;
 											PCSrc = 1'b0;
 											ALUSrc = 1'b0;
@@ -368,6 +438,7 @@ module control;
 											MemtoReg = 1'b0;
 											writeAddr = instruction[20:16];
 											half = 1'b1;
+											mux2Select = 2'b00;
 										 end
 		endcase
     case (funct)
@@ -379,7 +450,7 @@ module control;
       6'b10_1010 : begin ALUOpCode = 4'b0111; end
       default : begin ALUOpCode = 4'b0000; end
     endcase
-    $display("PC=%b instruction=%b writeRegData=%b",PC,instruction,writeRegData);
+		PC =temp_PC;
   end
 	always @ ( negedge clk ) begin
 		case (opcode)
@@ -423,6 +494,6 @@ module control;
 											RegWrite = 1'b0;
 										 end
 		endcase
-		PC = PCplus4;
+		$display("time=%d PC=%b opcode=%b temp_PC=%b",$time,PC,opcode,temp_PC);
 	end
 endmodule
--- mux3to1.v
+++ mux3to1.v
@@ -1,28 +1,58 @@
-module mux3to1(sel, i1, i2, i3, o1);
-  input [1:0] sel;
-  input [15:0] i1, i2, i3;
-  output [15:0] o1;
-  reg [15:0] o1;
+module mux3to1_1bit(input [1:0] sel, input in1, input in2, input in3, output out);
+  wire notSel1, notSel2, A, B, C;
+  not(notSel1, sel[0]);
+  not(notSel2, sel[1]);
+  and(A, in1, notSel1, notSel2);
+  and(B, in2, notSel2, sel[0]);
+  and(C, in3, sel[1], notSel1);
+  or(out, A, B, C);
+endmodule
 
-  always @(sel or i1 or i2 or i3) begin
-    case (sel)
-      2'b00: o1 = i1;
-      2'b01: o1 = i2;
-      2'b10: o1 = i3;
-    endcase
-  end
+module mux3to1(input [1:0] sel, input [31:0] A, input [31:0] B, input [31:0] C, output [31:0] result);
+  mux3to1_1bit mux1(sel, A[0], B[0], C[0], result[0]);
+  mux3to1_1bit mux2(sel, A[1], B[1], C[1], result[1]);
+  mux3to1_1bit mux3(sel, A[2], B[2], C[2], result[2]);
+  mux3to1_1bit mux4(sel, A[3], B[3], C[3], result[3]);
+  mux3to1_1bit mux5(sel, A[4], B[4], C[4], result[4]);
+  mux3to1_1bit mux6(sel, A[5], B[5], C[5], result[5]);
+  mux3to1_1bit mux7(sel, A[6], B[6], C[6], result[6]);
+  mux3to1_1bit mux8(sel, A[7], B[7], C[7], result[7]);
+  mux3to1_1bit mux9(sel, A[8], B[8], C[8], result[8]);
+  mux3to1_1bit mux10(sel, A[9], B[9], C[9], result[9]);
+  mux3to1_1bit mux11(sel, A[10], B[10], C[10], result[10]);
+  mux3to1_1bit mux12(sel, A[11], B[11], C[11], result[11]);
+  mux3to1_1bit mux13(sel, A[12], B[12], C[12], result[12]);
+  mux3to1_1bit mux14(sel, A[13], B[13], C[13], result[13]);
+  mux3to1_1bit mux15(sel, A[14], B[14], C[14], result[14]);
+  mux3to1_1bit mux16(sel, A[15], B[15], C[15], result[15]);
+  mux3to1_1bit mux17(sel, A[16], B[16], C[16], result[16]);
+  mux3to1_1bit mux18(sel, A[17], B[17], C[17], result[17]);
+  mux3to1_1bit mux19(sel, A[18], B[18], C[18], result[18]);
+  mux3to1_1bit mux20(sel, A[19], B[19], C[19], result[19]);
+  mux3to1_1bit mux21(sel, A[20], B[20], C[20], result[20]);
+  mux3to1_1bit mux22(sel, A[21], B[21], C[21], result[21]);
+  mux3to1_1bit mux23(sel, A[22], B[22], C[22], result[22]);
+  mux3to1_1bit mux24(sel, A[23], B[23], C[23], result[23]);
+  mux3to1_1bit mux25(sel, A[24], B[24], C[24], result[24]);
+  mux3to1_1bit mux26(sel, A[25], B[25], C[25], result[25]);
+  mux3to1_1bit mux27(sel, A[26], B[26], C[26], result[26]);
+  mux3to1_1bit mux28(sel, A[27], B[27], C[27], result[27]);
+  mux3to1_1bit mux29(sel, A[28], B[28], C[28], result[28]);
+  mux3to1_1bit mux30(sel, A[29], B[29], C[29], result[29]);
+  mux3to1_1bit mux31(sel, A[30], B[30], C[30], result[30]);
+  mux3to1_1bit mux32(sel, A[31], B[31], C[31], result[31]);
 endmodule
 
 module tb_mux3to1;
-  reg [15:0] A, B, C;
+  reg [31:0] A, B, C;
   reg [1:0] select;
-  wire [15:0] result;
+  wire [31:0] result;
 
   mux3to1 uut(select, A, B, C, result);
   initial begin
-    A = 16'b0000_0000_0000_0000;
-    B = 16'b0000_0000_0000_0001;
-    C = 16'b0000_0000_0000_0010;
+    A = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+    B = 32'b0000_0000_0000_0000_0000_0000_0000_1111;
+    C = 32'b0001_0001_1111_0000_1111_0000_0000_0000;
     select = 2'b00;
     #10
     select = 2'b01;
